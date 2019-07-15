2a0d8ea00b2c7f8b0372bb51eb0a3bd2662b7f3c
songtm <565038419@qq.com>
2019-07-15 18:32:55 +0800

    +texture packer

--- Assets/ABSystem/Scripts/AssetBundle/Loaders/SpriteAtlasLoader.cs
+++ Assets/ABSystem/Scripts/AssetBundle/Loaders/SpriteAtlasLoader.cs
@@ -1,3 +1,4 @@
+using System.Collections;
 using UnityEngine;
 using UnityEngine.U2D;
 
@@ -6,6 +7,7 @@ namespace Tangzx.ABSystem
     public class SpriteAtlasLoader : MonoBehaviour
     {
         private AssetBundleManager _bundleManager;
+        private int i = 0;
 
         void OnEnable()
         {
@@ -19,12 +21,20 @@ namespace Tangzx.ABSystem
 
         void RequestAtlas(string tag, System.Action<SpriteAtlas> callback)
         {
+
             if (_bundleManager == null) _bundleManager = AssetBundleManager.Instance;
             _bundleManager.Load(tag + ".spriteatlas", info =>
             {
+                i++;
                 var spriteAtlas = info.Require<SpriteAtlas>(this); //TODO:这里什么时候释放 AB 呢?
-                callback(spriteAtlas);
+                StartCoroutine(Delay(0.9f + i, callback, spriteAtlas));
             });
+        }
+
+        IEnumerator Delay(float sec, System.Action<SpriteAtlas> callback, SpriteAtlas atlas)
+        {
+            yield return  new WaitForSeconds(sec);
+            callback(atlas);
         }
     }
 }
